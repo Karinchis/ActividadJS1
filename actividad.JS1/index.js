7021ab1664e1464b7c7c5ba0c1c79135d3f42fab
Karinchis <98172386+Karinchis@users.noreply.github.com>
2022-04-16 20:15:45 -0500

    Add files via upload

---
+++ actividad.JS1/index.js
@@ -0,0 +1,10 @@
+function sumar() {
+var num1 = document.getElementById('x').value;
+var num2 = document.getElementById('y').value;
+var suma = parseInt(num1) + parseInt(num2);
+
+
+alert("La suma es: "+ suma);
+
+
+}
